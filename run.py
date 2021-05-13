66c240fb2261f007804bce47daa07302d74b9681
hellyryan1995 <helly.ryan1995@gmail.com>
2021-05-13 20:28:18 +0000

    Add validation to check for exactly 6 values in provided data

--- run.py
+++ run.py
@@ -22,7 +22,24 @@ def get_sales_data():
     print("example: 10,20,30,40,50,60\n")
 
     data_str = input("Enter your data here: ")
-    print(f"the data provided is {data_str}") 
+
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    """
+    Inside the try, converts all string value into integers raises ValueError 
+    if string cannot be conveted into int,
+    or if there arent exactly 6 values.
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+            f"Exactly 6 values required, you provided {len(values)}"
+        )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
 
 
 get_sales_data()
